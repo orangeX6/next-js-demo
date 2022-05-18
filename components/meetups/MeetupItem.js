c260d7dfa8e1795abe70636353b2b72b2ac3be5b
orangeX6 <46398819+orangeX6@users.noreply.github.com>
2022-05-18 23:28:42 +0530

    Update MeetupItem.js

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import Link from 'next/Link';
+import Link from 'next/link';
 
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
